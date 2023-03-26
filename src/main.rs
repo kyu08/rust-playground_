f513b82558865ef0a7008668a5a3ed88fa8da25a
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-26 23:18:40 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![1, 2, 3];
+
+    if vec1 == vec2 {
+        println!("The vectors are equal");
+    } else {
+        println!("The vectors are not equal");
+    }
+}
+
