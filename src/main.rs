a3d142fa904d677b74fb168cc98ae51611d880f9
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-09-23 22:55:20 +0900

    colored

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
+use colored::*;
+
 fn main() {
-    let vec1 = vec![1, 2, 3];
-    let vec2 = vec![1, 2, 3];
-
-    if vec1 == vec2 {
-        println!("The vectors are equal");
-    } else {
-        println!("The vectors are not equal");
-    }
+    println!("{}", ("make ".to_string() + &"run".to_string()).blue());
 }
-
